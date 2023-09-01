5dd887c444ee75d47120a984993840837245fe96
erteduarda <erteduarda@gmail.com>
2023-08-31 23:58:57 -0300

    Iniciando e finalizando cronometro.

---
+++ alura-tracker/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <main class="columns is-gapless is-multiline">
+    <div class="column is-one-quarter">
+      <BarraLateral />
+    </div>
+    <div class="column is-three-quarter">
+      <Formulario_ />
+    </div>
+  </main>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+import BarraLateral from './components/BarraLateral.vue';
+import Formulario_ from './components/Formulario.vue';
+
+export default defineComponent({
+  name: 'App',
+  components: {
+    BarraLateral,
+    Formulario_
+  }
+});
+
+</script>
+
+<style scoped></style>
