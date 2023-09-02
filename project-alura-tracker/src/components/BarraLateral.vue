65962c48b7a16c74780aad916b5c570d8e0643be
erteduarda <erteduarda@gmail.com>
2023-09-02 12:32:33 -0300

    Configurando estilos

--- project-alura-tracker/src/components/BarraLateral.vue
+++ project-alura-tracker/src/components/BarraLateral.vue
@@ -3,6 +3,9 @@
         <h1 class="is-flex is-justify-content-center">
             <img src="../assets/logo.png" alt="">
         </h1>
+        <button class="button" @click="alterarTema">
+            {{ textoDoBotao }}
+        </button>
     </header>
 </template>
 
@@ -10,7 +13,28 @@
 import { defineComponent } from "vue";
 
 export default defineComponent({
-    name: 'BarraLateral'
+    name: 'BarraLateral',
+    emits: ['aoAlterarTema'],
+    data () {
+        return {
+            modoEscuroAtivo: false
+        }
+    },
+    computed: {
+        textoDoBotao () {
+            if(this.modoEscuroAtivo) {
+                return 'Desativar modo escuro'
+            } else {
+                return 'Ativar modo escuro'
+            }
+        }
+    },
+    methods: {
+        alterarTema () {
+            this.modoEscuroAtivo = !this.modoEscuroAtivo
+            this.$emit('aoAlterarTema', this.modoEscuroAtivo)
+        }
+    }
 })
 </script>
 
@@ -20,6 +44,7 @@ header {
     background: #0d3b66;
     width: 100%;
     height: 100vh;
+    text-align: center;
 }
 
 @media only screen and (max-width: 768px) {
